e0f251079ce11a5d6ba4f83bb8724771175afee7
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-27 00:07:33 -0300

    base de datos creada desde models con el cli de sequelize

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const routes = require('./src/routes/index.js')
+const { sequelize } = require('./src/models/index.js')
 const app = express()
 
 app.use(express.json())
@@ -13,4 +14,8 @@ const PORT = process.env.PORT || 8082
 
 app.listen(PORT, () => {
     console.log(`Escuchando servidor en el puerto ${PORT}`)
+
+    sequelize.authenticate().then(()=>{
+        console.log('Nos hemos conectado a la base de datos!')
+    }) 
 })

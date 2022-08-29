e768cc22d595e209f5292125a30b1b539b53ca14
Pablo <delatorre.pablodaniel@gmail.com>
2022-08-29 02:49:23 -0300

    login funcionando

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -8,6 +8,35 @@ module.exports = {
     //Login
     signIn(req,res) {
 
+        let { email, password } = req.body;
+
+        User.findOne({
+            where:{
+                email:email
+            }
+        }).then(user => {
+
+            if(!user){
+                res.status(404).json({msg:"Usuario con este correo no encontrado"})
+            } else {
+                if(bcrypt.compareSync(password, user.password)){
+                    
+                    let token = jwt.sign({ user: user}, authConfig.secret, {
+                        expiresIn: authConfig.expires
+                    })
+
+                    res.json({
+                        user:user,
+                        token:token
+                    })
+                } else {
+                    res.status(401).json({msg:"Contraseña incorrecta"})
+                }
+            }
+        }).catch(err => {
+            res.status(500).json(err)
+        })
+
     },
 
     //Registro
